470b1bb7cbcadf407f672fb227aa2b4a6664466b
Andy Nadvorny <andynadvorny@gmail.com>
2021-02-26 12:53:26 -0300

    percentage fix

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -12,8 +12,8 @@ export function ExperienceBar() {
     <header className={styles.experienceBar}>
         <span>0 xp</span>
         <div>
-          <div style={{ width: `${percentToNextLevel}%` }} />
-          <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+        <div style={{ width: `${Number(percentToNextLevel)}%` }} />
+          <span className={styles.currentExperience} style={{ left: `${Number(percentToNextLevel)}%` }}>
             {currentExperience} xp
           </span>
         </div>
